e9e735a66b17ed053af3c74c2959fa969918b91d
aya misrabi <ayamisrabi@ayas-MacBook-Pro.local>
2022-06-04 16:46:39 -0700

    Amjad test

--- js/scripts.js
+++ js/scripts.js
@@ -229,7 +229,8 @@ var invite_response
 //            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc'){
 //            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
 //        } else {
-          $.post('https://script.google.com/macros/s/AKfycbwuJR7o1_X9zxcqcbjXAi1VCWyxQ5lTjMQom6MGbwWqCmisY7U/exec', data)
+//          $.post('https://script.google.com/macros/s/AKfycbwuJR7o1_X9zxcqcbjXAi1VCWyxQ5lTjMQom6MGbwWqCmisY7U/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbz5e-MnudHARBJUT8h4lI5wKdGRSEIZRdeh_A3yeK87Gb7UPhO2erh9LcZ_91bzhDp8pA/exec', data)
               .done(function (data) {
 //                  console.log(data);
 //                  console.log(invite_response);
